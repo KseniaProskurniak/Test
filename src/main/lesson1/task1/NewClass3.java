234a06207d0db7062082ff7323d1094044b4ff9c
Ksenia_Proskurnyak <proskurnyak@i-teco.ru>
2020-09-17 23:26:11 +0300

    Task2 +

--- src/main/lesson1/task1/NewClass3.java
+++ src/main/lesson1/task1/NewClass3.java
@@ -4,7 +4,7 @@ public class NewClass3 {
     public static void main(String[] args) {
         double radius = 100;
         double pi = 3.14;
-        double area = radius * radius * 3.14;
+        double area = radius * radius * pi;
         System.out.println("Result:" + " " + area);
     }
 }

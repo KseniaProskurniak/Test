ef1f251d1e206b2af72f652dd109588834e705d3
Ksenia_Proskurnyak <proskurnyak@i-teco.ru>
2020-09-17 22:09:17 +0300

    Test

--- src/main/lesson1/task1/NewClass.java
+++ src/main/lesson1/task1/NewClass.java
@@ -2,7 +2,8 @@ package main.lesson1.task1;
 
 public class NewClass {
     public static void main(String[] args) {
-        System.out.println(3);
+        float newNumber = 14.5f;
+        System.out.println("Результат =" + " " + newNumber);
     }
 
 }

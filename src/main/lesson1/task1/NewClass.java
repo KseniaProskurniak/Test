cac95269a63e2bebb898c8274146a5c5813acfaa
Ksenia_Proskurnyak <proskurnyak@i-teco.ru>
2020-09-17 21:42:24 +0300

    Test

---
+++ src/main/lesson1/task1/NewClass.java
@@ -0,0 +1,4 @@
+package main.lesson1.task1;
+
+public class NewClass {
+}
---
+++ src/main/lesson1/task1/Solution.java
@@ -0,0 +1,7 @@
+package main.lesson1.task1;
+
+public class Solution {
+    public static void main(String[] args) {
+        //что-то сделали
+    }
+}

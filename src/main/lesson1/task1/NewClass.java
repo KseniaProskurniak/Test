40e34a4b55ab9d9027eb20213e8867b71edf0fee
Ksenia_Proskurnyak <proskurnyak@i-teco.ru>
2020-09-17 22:05:24 +0300

    Test

--- src/main/lesson1/task1/NewClass.java
+++ src/main/lesson1/task1/NewClass.java
@@ -1,7 +1,8 @@
 package main.lesson1.task1;
 
 public class NewClass {
-    int a =1;
-
+    public static void main(String[] args) {
+        System.out.println(3);
+    }
 
 }
--- src/main/lesson1/task1/Solution.java
+++ src/main/lesson1/task1/Solution.java
@@ -2,6 +2,5 @@ package main.lesson1.task1;
 
 public class Solution {
     public static void main(String[] args) {
-        //что-то сделали
-    }
+}
 }
